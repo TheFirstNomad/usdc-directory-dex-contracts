8a77bf83c92e4acfea635405e64e97ad421d459a
TheFirstNomad <119462780+TheFirstNomad@users.noreply.github.com>
2026-04-09 01:13:42 +0300

    Create ArcFactory.sol

---
+++ ArcFactory.sol
@@ -0,0 +1,35 @@
+pragma solidity =0.5.16;
+
+import './ArcPair.sol';
+
+contract ArcFactory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'ArcDEX: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'ArcDEX: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'ArcDEX: PAIR_EXISTS');
+
+        pair = address(new ArcPair());
+        IArcPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair;
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+}
+
+interface IArcPair {
+    function initialize(address, address) external;
+}
